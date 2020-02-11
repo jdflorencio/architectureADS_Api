e076411e210ef2e1fbf93c2ffaac1a201320f4bc
João Diego <jdflorencio@gmail.com>
2020-02-11 16:57:16 -0300

    alaterando o nome da rota

--- modules/notaFiscal/notaFiscal.router.js
+++ modules/notaFiscal/notaFiscal.router.js
@@ -1,10 +1,10 @@
 const router = require('express').Router()
 const controller = require('./notaFiscal.controller')
 
-router.get('/notaFiscal', async (req, res) => { await controller.findAll(req, res) })
-router.get('/notaFiscal/:id', async (req, res) => { await controller.findOne(req, res) })
-router.post('/notaFiscal', async (req, res) => { await controller.save(req, res) })
-router.put('/notaFiscal', async (req, res) => { await controller.update(req, res) })
-router.delete('/notaFiscal/:id', async (req, res) => { await controller.delete(req, res) })
+router.get('/notafiscal', async (req, res) => { await controller.findAll(req, res) })
+router.get('/notafiscal/:id', async (req, res) => { await controller.findOne(req, res) })
+router.post('/notafiscal', async (req, res) => { await controller.save(req, res) })
+router.put('/notafiscal', async (req, res) => { await controller.update(req, res) })
+router.delete('/notafiscal/:id', async (req, res) => { await controller.delete(req, res) })
 
 module.exports = router
