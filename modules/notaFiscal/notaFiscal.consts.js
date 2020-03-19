bc00e9a299a88e7350cf41d921f1464ffa8b992f
João Diego <jdflorencio@gmail.com>
2020-03-19 09:32:47 -0300

    ativando permissoes para criar as notas

---
+++ modules/notaFiscal/notaFiscal.consts.js
@@ -0,0 +1,10 @@
+const role = {
+    acessar:  "NOTA010",
+    acessarId: "NOTA011",
+    editar :  "NOTA020",
+    deletar:  "NOTA030",
+    cadastrar : "NOTA040"   
+}
+
+exports.module = role
+
--- modules/notaFiscal/notaFiscal.router.js
+++ modules/notaFiscal/notaFiscal.router.js
@@ -1,10 +1,13 @@
 const router = require('express').Router()
 const controller = require('./notaFiscal.controller')
+const mid = require('../../core/permissions')
 
-router.get('/notafiscal', async (req, res) => { await controller.findAll(req, res) })
-router.get('/notafiscal/:id', async (req, res) => { await controller.findOne(req, res) })
-router.post('/notafiscal', async (req, res) => { await controller.save(req, res) })
-router.put('/notafiscal', async (req, res) => { await controller.update(req, res) })
-router.delete('/notafiscal/:id', async (req, res) => { await controller.delete(req, res) })
+let permissao = require('../notaFiscal/notaFiscal.consts')
+
+router.get('/notafiscal', mid.middleware(permissao.acessar),async (req, res) => { await controller.findAll(req, res) })
+router.get('/notafiscal/:id', mid.middleware(permissao.acessarId),async (req, res) => { await controller.findOne(req, res) })
+router.post('/notafiscal', mid.middleware(permissao.cadastrar),async (req, res) => { await controller.save(req, res) })
+router.put('/notafiscal', mid.middleware(permissao.editar),async (req, res) => { await controller.update(req, res) })
+router.delete('/notafiscal/:id', mid.middleware(permissao.deletar),async (req, res) => { await controller.delete(req, res) })
 
 module.exports = router
