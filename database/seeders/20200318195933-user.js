a57de9387681d76882374cec84e116d7548aeda3
João Diego <jdflorencio@gmail.com>
2020-03-19 09:29:05 -0300

    dados para testes

--- database/seeders/20200318195933-user.js
+++ database/seeders/20200318195933-user.js
@@ -6,11 +6,13 @@ const saltRounds = 10
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('user', [{
+      id: 1,
       name: "João Diego",
       email: "joaodiego@gmail.com",
       password: bcrypt.hashSync('123456', saltRounds)
     },
     {
+      id: 2,
       name: "Teste",
       email: "teste@gmail.com",
       password: bcrypt.hashSync('123456', saltRounds)
