b1c5a781cb58e8d6d9a461082eb96470a5c34848
Joao Diego <jdflorencio@gmail.com>
2020-05-13 15:11:21 -0300

    migrate grupo

--- database/migrations/20200513125818-create-grupo.js
+++ database/migrations/20200513125818-create-grupo.js
@@ -2,18 +2,50 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
+    return queryInterface.createTable('grupo', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        allowNull: false,
+        references: {
+          model: 'user',
+          key: 'id'
+        },
+        type: Sequelize.INTEGER,
+      },
+      pessoaId: {
+        allowNull: false,
+        references: {
+          model: 'pessoa',
+          key: 'id'
+        },
+        type: Sequelize.INTEGER,
+      },
+      descricao: {
+        type: Sequelize.STRING(30)
+      },
+      log_criacao: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      log_atualizacao: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      log_pct_usuario: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      }
+    })
   },
 
   down: (queryInterface, Sequelize) => {
     /*
-      Add reverting commands here.
+      Add reverting commands here.'
       Return a promise to correctly handle asynchronicity.
 
       Example:
