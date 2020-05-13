d61cc1761775c3ede60c079a0210f6a42148fac9
Joao Diego <jdflorencio@gmail.com>
2020-05-13 11:33:25 -0300

    migration telefone...

--- database/migrations/20200513125808-create-telefone.js
+++ database/migrations/20200513125808-create-telefone.js
@@ -2,22 +2,53 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
+    return queryInterface.createTable('telefone', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        allowNull: false,
+        references: {
+          model: 'user',
+          key: 'id'
+        },
+        type: Sequelize.INTEGER,
+      },
+      pessoaId: {
+        allowNull: false,
+        references: {
+          model: 'pessoa',
+          key: 'id'
+        },
+        type: Sequelize.INTEGER,
+      },
+      telefone: {
+        allowNull: false,
+        defaultValue: null,
+        type: Sequelize.STRING(255)
+      },
+      tipo: {
+        allowNull: false,
+        type: Sequelize.STRING(10)
+      },
+      log_criacao: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      log_atualizacao: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      log_pct_usuario: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      }
+    })
   },
-
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
+    return queryInterface.dropTable('telefone')
   }
 };
