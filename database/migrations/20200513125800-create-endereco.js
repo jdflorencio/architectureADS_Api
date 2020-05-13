b0253035e84f3bf36705e2be6d08ca50a75631a3
Joao Diego <jdflorencio@gmail.com>
2020-05-13 11:09:32 -0300

    adicionando o migrate de endereco

--- database/migrations/20200513125800-create-endereco.js
+++ database/migrations/20200513125800-create-endereco.js
@@ -1,23 +1,76 @@
 'use strict';
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
 
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('endereco', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        allowNull: false,
+        references: {
+          model: 'user',
+          key: 'id'
+        },
+        type: Sequelize.INTEGER,
+      },
+      pessoaId: {
+        allowNull: false,
+        references: {
+          model: 'pessoa',
+          key: 'id'
+        },
+        type: Sequelize.INTEGER,
+      },
+      endereco: {
+        allowNull: false,
+        type: Sequelize.STRING(255)
+      },
+      bairro: {
+        allowNull: true,
+        defaultValue: null,
+        type: Sequelize.STRING(60)
+      },
+      numero: {
+        allowNull: true,
+        defaultValue: 0,
+        type: Sequelize.STRING(10)
+      },
+      complemento: {
+        allowNull: true,
+        defaultValue: null,
+        type: Sequelize.STRING(255)
+      },
+      cidade: {
+        allowNull: true,
+        defaultValue: null,
+        type: Sequelize.STRING(60)
+      },
+      uf: {
+        allowNull: true,
+        defaultValue: null,
+        type: Sequelize.STRING(2)
+      },
+      log_criacao: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      log_atualizacao: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      log_pct_usuario: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      }
+    })
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
+    return queryInterface.dropTable('endereco')
   }
 };
