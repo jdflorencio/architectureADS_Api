ec80f8e1fb72a909d863bee1a9b242496ecf7e01
João Diego <jdflorencio@outlook.com>
2020-06-28 12:29:27 -0300

    exemplo de como ficarao padronizando os controllers

--- modules/grupo/grupo.controller.js
+++ modules/grupo/grupo.controller.js
@@ -1,64 +1,10 @@
 const grupoService = require('./grupo.service')
-const Response = require('../../core/response')
-class GrupoController {
+const Response = require('../../core/response');
+const BaseController = require('../../core/baseController');
+class GrupoController extends BaseController{
 	constructor() {
-		this.service = grupoService
-	}
-
-	async save(req, res) {
-		try {
-			const result = await this.service.save(req.body);
-			new Response(res).success(result)
-
-		} catch (error) {
-			new Response(res).preConditionFailed(error)
-		}
-	}
-
-	async update(req, res) {
-		try {
-
-			const result = await this.service.update(req.body);
-			new Response(res).success(result)
-
-		} catch (error) {
-			new Response(res).preConditionFailed(error)
-		}
-	}
-
-	async findOne(req, res) {
-		try {
-
-			const result = await this.service.findById(+req.params.id);
-			new Response(res).success(result)
-
-		} catch (error) {
-			new Response(res).preConditionFailed(error)
-
-		}
-	}
-
-	async findAll(req, res) {
-		try {
-			const result = await this.service.findAll();
-
-			new Response(res).success(result)
-
-		} catch (error) {
-			new Response(res).preConditionFailed(error)
-		}
-	}
-
-	async delete(req, res) {
-		try {
-			const { id } = req.params
-			await this.service.deleting(id)
-			new Response(res).success(id, `Grupo codigo: ${id} removido Com sucesso!`)
-
-		} catch (error) {
-			res.status(409).json({ sucesso: false, erro: error })
-		}
-	}
+		super(service)
+	}	
 }
 
 let grupo = new GrupoController();
