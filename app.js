249bc8df3894e57883bcf978a20545546cde3492
João Diego <jdflorencio@outlook.com>
2020-07-10 10:09:59 -0300

    Carregando de modo automtico os resolvers e schemas do GraphQL

--- app.js
+++ app.js
@@ -2,14 +2,12 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const cors = require('cors')
-const { ApolloServer, gql } = require('apollo-server-express')
-const resolvers = require('./graphQL/resolvers')
-const typeDefs  = require('./graphQL/schemas')
-
+const path = require('path')
 const authorization = require('./auth/auth')
 
-
-
+const { ApolloServer } = require('apollo-server-express')
+const { loadFilesSync } = require('@graphql-tools/load-files')
+const { mergeTypeDefs, mergeResolvers } = require('@graphql-tools/merge')
 
 const rotaPessoa = require('./modules/pessoa/pessoa.router')
 const rotaGrupo = require('./modules/grupo/grupo.router')
@@ -19,27 +17,33 @@ const rotaTributacao = require('./modules/tributacao/tributacao.router')
 const rotaNotafiscal = require('./modules/notaFiscal/notaFiscal.router')
 const rotaLogin = require('./modules/login/login.router')
 
+function carregaTodos(padraoBusca) {
+  return loadFilesSync(path.join(__dirname, padraoBusca))    
+}
+
+const definicoesTipos =  carregaTodos('./modules/**/*.graphql')
+const resolvers =  carregaTodos('./modules/**/*.resolvers.js')
+
 /* CONFIG */
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use('/api', rotaLogin)
-
 app.use(authorization.readAuthorization)
 
-const server = new ApolloServer({ typeDefs, resolvers, 
-  context: ({ req }) => {
-    
-    
+const apolloServer = new ApolloServer({
+  typeDefs: mergeTypeDefs(definicoesTipos, { all: true }),
+  resolvers: mergeResolvers(resolvers, { all: true }), 
+
+  context: ({req}) =>{
     const credenciais = req.credenciais
 
-    return { credenciais };
+    return {credenciais}
   }
-  
 })
-
-server.applyMiddleware({ app })
+  
+apolloServer.applyMiddleware({ app })
 
 app.use('/api', rotaPessoa)
 app.use('/api', rotaGrupo)
@@ -63,6 +67,8 @@ app.use((error, req, res, next) => {
     }
   })
 })
+
+
 
 
 
--- graphQL/resolvers.js
+++
@@ -1,8 +0,0 @@
-const { mergeResolvers } = require('graphql-tools')
-
-const pessoaResolver = require('../modules/pessoa/pessoa.resolvers')
-const resolvers = [
-    pessoaResolver
-]
-
-module.exports = mergeResolvers(resolvers);
